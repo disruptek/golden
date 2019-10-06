692e487f49de109483d14701e71ec84def39a879
Andy Davidoff <andy+github@disruptek.com>
2019-10-06 19:24:22 -0400

    handle your own output

--- src/golden/benchmark.nim
+++ src/golden/benchmark.nim
@@ -4,8 +4,11 @@ stuff related to the BenchmarkResult and benchmarking in a broad sense
 
 ]#
 import os
+import times
 import strutils
+
 import spec
+import output
 import running
 
 type
@@ -13,6 +16,14 @@ type
     binary*: FileDetail
     compilations*: RunningResult[CompilationInfo]
     invocations*: RunningResult[InvocationInfo]
+
+proc `$`*(bench: BenchmarkResult): string =
+  result = $bench.GoldObject
+  if bench.invocations.len > 0:
+    let invocation = bench.invocations.first
+    result &= "\n" & $invocation
+  result &= "\ncompilation(s) -- " & $bench.compilations
+  result &= "\n invocation(s) -- " & $bench.invocations
 
 proc newBenchmarkResult*(): BenchmarkResult =
   new result
@@ -28,3 +39,17 @@ proc appearsBenchmarkable*(path: string): bool =
   if detail.info.kind notin {pcFile, pcLinkToFile}:
     return false
   result = true
+
+proc output*(golden: Golden; benchmark: BenchmarkResult; desc: string = "") =
+  ## generally used to output a benchmark result periodically
+  let since = getTime() - benchmark.entry.toTime
+  golden.output desc & " after " & $since.inSeconds & "s"
+  while true:
+    golden.output $benchmark
+    var
+      dims = benchmark.invocations.wall.makeDimensions(12)
+      histo = benchmark.invocations.crudeHistogram(dims)
+    if benchmark.invocations.maybePrune(histo, dims, 0.001):
+      continue
+    golden.output $histo
+    break
