61a100f710baf0442494e2a443a9120b5aa02d4e
Andy Davidoff <andy+github@disruptek.com>
2019-10-06 14:41:30 -0400

    some notes

--- src/golden/git.nim
+++ src/golden/git.nim
@@ -1,11 +1,42 @@
+#[
+
+this is a long rabbithole, so focus a bit on the target.
+
+we want to be able to add metadata
+  - for a file
+    - about all we can determine is that it's versioned
+      and it's modified or not; changes to the repo/env
+      invalidate all other assumptions
+  - for a repo
+    - so we can reproduce builds or confirm binary identity
+
+we want to to use that metadata to make decisions and take action
+  - roll the compiler
+  - roll the target
+
+]#
+
 import nimgit2
 
+{.hint: "libgit2 version " & git2SetVer.}
+
+proc dumpError() =
+  let err = git_error_last()
+  if err == nil:
+    return
+  stdmsg().writeLine "\"" & $err.message & "\""
+  quit(err.klass)
+
 proc init*() =
-  let lg = git_libgit2_init()
-  if lg != 0:
-    echo "unable to initialize libgit2; error code " & $lg
-    when declared(git_error_last):
-      let err = git_error_last()
-      echo "message: " & err.message
-    else:
-      {.warning: "git_error_last() not found".}
+  let count = git_libgit2_init()
+  if count > 0:
+    return
+  assert count != 0, "unable to initialize libgit2; no error code!"
+  dumpError()
+
+proc shutdown*() =
+  let count = git_libgit2_shutdown()
+  if count == 0:
+    return
+  assert count > 0, $count & " too many git inits"
+  dumpError()
