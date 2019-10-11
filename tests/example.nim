3930cd85f994ec28f96fe903686cdf2e7729d395
Andy Davidoff <andy+github@disruptek.com>
2019-10-11 17:33:40 -0400

    fixup tests

--- tests/example.nim
+++ tests/example.nim
@@ -4,4 +4,10 @@ import os
 if paramCount() == 1 and paramStr(1) == "quit":
   quit(3)
 
+if paramCount() == 1 and paramStr(1) == "hello":
+  echo "world"
+
+if paramCount() == 1 and paramStr(1) == "goodbye":
+  stderr.writeLine "cruel world"
+
 sleep 100
--- tests/tinvoke.nim
+++ tests/tinvoke.nim
@@ -1,21 +1,45 @@
+import os
 import asyncdispatch
 import unittest
 
 import golden
 import golden/spec
 import golden/invoke
+import golden/compilation
 
-suite "invoke":
+suite "compile and invoke":
   setup:
-    let compilation = waitfor compileFile("tests/example.nim")
-    check compilation.okay
+    let
+      exampleNim = newFileDetailWithInfo("tests/example.nim")
+      compilation = newCompilationInfo(exampleNim.path)
+      binary {.used.} = compilation.binary
+
+  test "assumptions":
+    check compilation.compiler.chash != ""
+    check compilation.compiler.version != ""
+    check argumentsForCompilation(@[]) == @["c", "-d:danger"]
+    check argumentsForCompilation(@["umm"]) == @["c", "umm"]
+    check argumentsForCompilation(@["cpp", "-d:debug"]) == @["cpp", "-d:debug"]
 
   test "compilation":
-    check compilation.okay
+    let
+      target = pathToCompilationTarget(exampleNim.path)
+    if fileExists(target):
+      removeFile(target)
+    let
+      simple = waitfor compileFile(exampleNim.path)
+    check simple.okay
+    check compilation.binary.path == target
+    check compilation.binary.path.fileExists
 
   test "invocation":
-    var invocation = waitfor invoke(compilation.binary)
+    var invocation = waitfor invoke(binary)
     check invocation.okay
-    invocation = waitfor invoke(compilation.binary, @["quit"])
+    invocation = waitfor invoke(binary, @["quit"])
     check not invocation.okay
     check invocation.output.code == 3
+    invocation = waitfor invoke(binary, @["hello"])
+    check invocation.okay
+    check invocation.output.stdout == "world\n"
+    invocation = waitfor invoke(binary, @["goodbye"])
+    check invocation.output.stderr == "cruel world\n"
